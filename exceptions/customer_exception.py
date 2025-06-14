af5caaec8917577aa72bc8727f50dbb0fb93957a
AngelOrtizTorres <4ngel0rtiz7orres0330@gmail.com>
2025-06-14 17:15:20 +0200

    Add custom exception classes for DNI and customer status validation

--- exceptions/customer_exception.py
+++ exceptions/customer_exception.py
@@ -12,3 +12,11 @@ class LetterErrorDNI(ValidationException):
 class FormatErrorPhone(ValidationException):
     def __init__(self):
         super().__init__("El número es incorrecto, debe contener 9 dígitos")
+
+class DNINotFoundError(ValidationException):
+    def __init__(self):
+        super().__init__("El DNI no se ha encontrado")
+
+class CustomerInactiveError(ValidationException):
+    def __init__(self, *args):
+        super().__init__("El cliente se encuentra inactivo en este momento")
