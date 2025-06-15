17235a44ab4ae36cf1e728f7f10732666636a0b4
AngelOrtizTorres <4ngel0rtiz7orres0330@gmail.com>
2025-06-15 19:04:50 +0200

    Remove unused abstract methods from MovementsDAO class

--- movements/movementsDao.py
+++ movements/movementsDao.py
@@ -6,19 +6,3 @@ class MovementsDAO(ABC):
     @abstractmethod
     def create_movement(self, movement):
         pass
-
-    @abstractmethod
-    def get_deposit(self):
-        pass
-
-    @abstractmethod
-    def get_withdraw(self):
-        pass
-
-    @abstractmethod
-    def get_transfer(self):
-        pass
-
-    @abstractmethod
-    def get_movements_between_date(self):
-        pass
